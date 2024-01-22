ef2b8b60dfa2ced2d61bfcdf45b1ec4c1b7e8fc5
Nate <nate.foulkes@hotmail.com>
2024-01-22 14:20:06 -0500

    munge.r

--- munge.r
+++ munge.r
@@ -4,53 +4,55 @@ library(janitor)
 library(skimr)
 # data ----
 df <- read_csv("./train.csv",
-               na = c("NA"),
-               col_types = cols(.default = col_character()))
-# munge ----
+               na = c("NA"))
+# munge 
 df <- df |>
     clean_names() # janitor
 
+df <- df |> 
+    mutate(
+        across(everything(), as.character)) |>
+    mutate(
+        across(where(is.character), str_to_lower))
 # numeric -----------------------------------------------------------------
 
-df <- df |>
-    mutate(
-        across(where(is.character), str_to_lower)
-        ) |> 
+df <- df |> 
     mutate(
         across(c(lot_frontage, lot_area, mas_vnr_area, bsmt_fin_sf1,
-                    bsmt_fin_sf2, bsmt_unf_sf, total_bsmt_sf, x1st_flr_sf,
-                    x2nd_flr_sf, low_qual_fin_sf, gr_liv_area, 
-                    garage_area, wood_deck_sf, open_porch_sf, enclosed_porch,
-                    x3ssn_porch, screen_porch, pool_area, misc_val, sale_price), 
+                 bsmt_fin_sf2, bsmt_unf_sf, total_bsmt_sf, 
+                 x1st_flr_sf, x2nd_flr_sf, low_qual_fin_sf, gr_liv_area, 
+                 garage_area, wood_deck_sf, open_porch_sf, enclosed_porch,
+                 x3ssn_porch, screen_porch, pool_area, misc_val, sale_price,
+                 year_built, year_remod_add), 
                   ~ as.numeric(.)))
 
 
 # missing_values ----------------------------------------------------------
-# # missing numeric ----
-# # lot_frontage 
-average_lot_frontage_by_lot_shape <- df |> 
-    filter(!is.na(lot_frontage)) |> 
-    group_by(lot_shape) |> 
-    summarise(mean_lot_frontage = round(mean(lot_frontage),1))
+
+# # lot_frontage ----
+# table for average values
+# average_lot_frontage_by_lot_shape <- df |>
+#     filter(!is.na(lot_frontage)) |>
+#     group_by(lot_shape) |>
+#     summarise(mean_lot_frontage = round(mean(lot_frontage),1))
 df <- df |>  
     mutate(impute_lot_frontage = case_when(lot_shape == "ir1" ~ 76.1,
                                            lot_shape == "ir2" ~ 76.5,
                                            lot_shape == "ir3" ~ 138.4,
-                                           lot_shape == "reg" ~ 67.0))
-df<- df |> 
+                                           lot_shape == "reg" ~ 67.0)) 
+df <- df |> 
     mutate(lot_frontage = coalesce(df$lot_frontage, df$impute_lot_frontage)) |> 
     select(-impute_lot_frontage) 
-# # mas_vnr_ area, type 
+# # mas_vnr_ area, type ----
 df <- df |> 
     mutate(across(mas_vnr_type, ~ replace_na(., "unknown")))
-
+# impute missing mas_vnr_area with mean of non-missing values
 df <- df |> 
     mutate(mas_vnr_area = case_when(
         mas_vnr_type == "unknown" ~ mean(df$mas_vnr_area, na.rm = TRUE),
         TRUE ~ mas_vnr_area))
-# # missing character/factors
-## bsmt
-# df |> select(total_bsmt_sf,contains("bsmt")) |> filter(total_bsmt_sf == 0) |> print(n = Inf)
+
+## bsmt ----
 df <- df |> 
     mutate(bsmt_qual = case_when(
         total_bsmt_sf == 0 & is.na(bsmt_qual) ~ "no basement",
@@ -74,11 +76,14 @@ df <- df |>
         total_bsmt_sf > 0 & bsmt_fin_sf2 > 0 ~ "unknown", # if there is no type 2 sf
         TRUE ~ bsmt_fin_type2
     )) 
-# values where na means none
+
+# alley_fence_misc_feature ------------------------------------------------
+
+# values where na is none
 df <- df |> 
     mutate(across(c(alley, fence, misc_feature), 
                   ~ replace_na(., "none")))
-# fireplace_qu
+# fireplace_qu ----
 # x <- df |> 
 #     filter(fireplaces == 0) |> 
 #     select(fireplace_qu) |> 
@@ -86,50 +91,88 @@ df <- df |>
 # after checking that fireplaces = 0, all fireplace_qu are na
 df <- df |> 
     mutate(across(fireplace_qu, ~ replace_na(., "none")))
-# electrical
+# electrical ----
 # the missing value appears to be a mistake since the other houses in that neighborhood built after 1946 all have sbrkr
 df <- df |> 
     mutate(across(electrical,
                   ~ replace_na(., "sbrkr")))
-# utilities 
+# utilities ----
 # utilities has 4 levels, but all in df are allpub except one nosewa
 df <- df |> 
     mutate(utilities = fct_lump_n(utilities, 
                                   n = 74, # one more than 5% of total
                                   other_level = "pub_missing")) |> 
     mutate(utilities = as.character(utilities))
-# pool_qc
+# pool_qc ----
 # df |> filter(pool_area == 0) |> count(pool_qc) # all na pool_qc have a pool_area = 0
 df <- df |> 
     mutate(across(pool_qc,
                   ~ replace_na(., "no pool")))
-# garage
+# garage ----
 df <- df |> 
     mutate(across(c(garage_type, garage_finish, garage_qual, garage_cond),
-                  ~ replace_na(., "no garage")))
-# df |> filter(is.na(garage_yr_blt)) |> select(garage_area) |> print(n = Inf)
-# since I can't figure out a good way to impute missing garage_yr_blt I will drop the column 
+                  ~ replace_na(., "no garage"))) |> 
+    mutate(has_garage = case_when(
+        garage_area > 0 ~ "has_garage",
+        garage_area == 0 ~ "no_garage")) 
+# garage_yr_blt missing values
+# new column determining the type of garage structure:
+# original = garage was built the same year as the house
+# add_on = garage was built after the house
+# no_garage = no existing garage
 df <- df |> 
+    mutate(
+        garage_str = 
+               case_when(
+                   year_built == garage_yr_blt ~ "original", 
+                   year_built != garage_yr_blt ~ "add_on", 
+                   is.na(garage_yr_blt) ~ "no_garage"
+        ))
+# unselect garage_yr_blt
+df <- df |>
     select(-garage_yr_blt)
-# ordered_factors ---------------------------------------------------------------
+
+# neighborhood ------------------------------------------------------------
+
+
+df <- df |> # combine two lowest rates 
+    mutate(neighborhood = case_when(
+        neighborhood == "Blueste" ~ "NPk_Blue",
+        neighborhood == "NPkVill" ~ "NPk_Blue",
+        TRUE ~ neighborhood
+    ))
+
+# has_remod ---------------------------------------------------------------
+
+df <- df|> 
+    mutate(has_remod = case_when(
+        year_built - year_remod_add == 0 ~ "no",
+        TRUE ~ replace_na("yes"))) |>
+    select(-year_remod_add) 
+
+
+# has_porch ---------------------------------------------------------------
+
 df <- df |> 
-    mutate(across(c(overall_qual,overall_cond),
-                  ~ ordered(., levels = 1:10)))
+    mutate(has_porch = if_else(
+        (open_porch_sf > 0 | enclosed_porch > 0 | x3ssn_porch > 0 | screen_porch > 0),
+        "yes", "no", missing = NULL
+    ))
+df <- df |> 
+    select(-c(contains("porch")))
 
-# convert_to_date ---------------------------------------------------------
+# has_deck ----------------------------------------------------------------
 
 df <- df |> 
-    mutate(date_sold = make_date(yr_sold, mo_sold)) |> 
-    select(-c(mo_sold, yr_sold)) |> 
-    mutate(year_built = make_date(year_built)) |> 
-    mutate(year_remod_add = make_date(year_remod_add))
+    mutate(has_deck = if_else(
+        wood_deck_sf > 0, "yes", "no", missing = NULL
+    ))
+df <- df |> 
+    select(-wood_deck_sf)
 
-# anonomize factors -------------------------------------------------------
+# has_pool ----------------------------------------------------------------
 
 df <- df |> 
-    select(-id) |> 
-    mutate(across(where(is.character), 
-                  ~ fct_anon(.)))
-
-# write_csv ----
-write_csv(df, "./mutated_df.csv")
+    mutate(has_pool = if_else(
+        pool_area > 0, "yes", "no", missing = NULL)) |> 
+    select(-pool_area)
